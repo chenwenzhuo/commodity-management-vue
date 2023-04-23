62a8e2a5c6a96e42b916f8b7c443811c6cc34602
chenwenzhuo <Chenwz1996@163.com>
2023-04-23 22:32:39 +0800

    路由守卫检查进入路由前是否已登陆

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from "@/views/Login";
+import store from "@/store";
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,15 @@ const routes = [
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
+        beforeEnter(to, from, next) {
+            //进入登陆页前检查，若已有登陆成功的用户，跳转到主页
+            if (store.state.logged_usr) {
+                next('/');
+                return;
+            }
+            next();
+        },
     },
     {
         path: '/',
@@ -16,7 +25,15 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Home')
+        component: () => import(/* webpackChunkName: "about" */ '../views/Home'),
+        beforeEnter(to, from, next) {
+            //进入主页前检查，若还未登陆，跳转到登陆页
+            if (!store.state.logged_usr) {
+                next('/login');
+                return;
+            }
+            next();
+        },
     }
 ]
 
