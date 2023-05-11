2fb24d9bd1383d45bbeebb9dfc4fcdcd2e429384
yatelu <yydys1224@163.com>
2023-05-11 11:51:42 +0800

    完成用户对菜单的访问权限控制

--- src/components/AsideNav.vue
+++ src/components/AsideNav.vue
@@ -11,25 +11,25 @@
                 background-color="#0e1520"
                 text-color="#ffffff"
                 active-text-color="#ffd04b">
-            <el-menu-item index="1">
+            <el-menu-item index="1" v-if="menuDisplay.home">
                 <template slot="title">
                     <i class="el-icon-s-home"></i>
                     <span>首页</span>
                 </template>
             </el-menu-item>
-            <el-submenu index="2">
+            <el-submenu index="2" v-if="menuDisplay.category||menuDisplay.product">
                 <template slot="title">
                     <i class="el-icon-menu"></i>
                     <span>商品</span>
                 </template>
-                <el-menu-item index="2-1">品类管理</el-menu-item>
-                <el-menu-item index="2-2">商品管理</el-menu-item>
+                <el-menu-item index="2-1" v-if="menuDisplay.category">品类管理</el-menu-item>
+                <el-menu-item index="2-2" v-if="menuDisplay.product">商品管理</el-menu-item>
             </el-submenu>
-            <el-menu-item index="3">
+            <el-menu-item index="3" v-if="menuDisplay.user">
                 <i class="el-icon-document"></i>
                 <span slot="title">用户管理</span>
             </el-menu-item>
-            <el-menu-item index="4">
+            <el-menu-item index="4" v-if="menuDisplay.role">
                 <i class="el-icon-setting"></i>
                 <span slot="title">角色管理</span>
             </el-menu-item>
@@ -43,7 +43,23 @@ import {mapMutations, mapState} from "vuex";
 export default {
     name: "AsideNav",
     computed: {
-        ...mapState(['active_menu_index'])
+        ...mapState(['active_menu_index', 'logged_usr']),
+        menuDisplay() {
+            //如果是admin用户，则有全部菜单权限
+            if (this.logged_usr.username === 'admin') {
+                return {
+                    home: true, category: true, product: true,
+                    user: true, role: true
+                }
+            }
+            return {
+                home: this.logged_usr.role.menus.includes('/home'),
+                category: this.logged_usr.role.menus.includes('/category'),
+                product: this.logged_usr.role.menus.includes('/product'),
+                user: this.logged_usr.role.menus.includes('/user'),
+                role: this.logged_usr.role.menus.includes('/role')
+            };
+        }
     },
     methods: {
         ...mapMutations(['set_active_menu', 'set_active_menu_index']),
